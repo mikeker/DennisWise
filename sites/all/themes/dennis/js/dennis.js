dc403de489035d71796ec919c49b2c898d557652
Mike Keran <info@mikekeran.com>
2014-09-10 21:22:39 +0200

    Basic structure for JS.

---
+++ sites/all/themes/dennis/js/dennis.js
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * dennis.js
+ *
+ * Provides site-wide JS for DennisWise.com.
+ */
+(function ($) {
+  Drupal.behaviors.dennis = {
+    attach: function(context) {
+
+    }
+  };
+}) (jQuery);
