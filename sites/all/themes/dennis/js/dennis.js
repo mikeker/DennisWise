d1ceaa7cba96b72ce81570eb33a4bf4b6301d1f4
Mike Keran <info@mikekeran.com>
2014-09-11 17:40:05 +0200

    Finished image aspect ratio work. Started forward/back click zones.

--- sites/all/themes/dennis/js/dennis.js
+++ sites/all/themes/dennis/js/dennis.js
@@ -42,8 +42,22 @@ var Dennis = Dennis || { photos: {} };
     // the images have been preloaded into the browser cache.
     this.preloaded = {};
 
+    // This the the HTML element that we're putting the images in
+    this.$display = $('.dennis-two-col .fluid-column');
+
     // Add a "Loading..." message to the photo area.
-    this.loading = $('<div class="photos-loading">Loading...</div>').prependTo('.dennis-two-col .fluid-column');
+    this.$loading = $('<div class="photos-loading">Loading...</div>').prependTo(this.$display);
+
+    // Add forward/back click areas
+    $('<div class="photos-forward"></div><div class="photos-backward"></div>').appendTo(this.$display);
+    $('.photos-forward', this.$display).click(function() {
+      this.next();
+      this.show();
+    });
+    $('.photos-backward', this.$display).click(function() {
+      this.prev();
+      this.show();
+    });
 
     this.init.completed = true;
   };
@@ -59,8 +73,58 @@ var Dennis = Dennis || { photos: {} };
    * Set the current image as the background of the "fluid" layout region.
    */
   Dennis.photos.show = function() {
-    $('.dennis-two-col .fluid-column')
-      .css('background-image', 'url(' + this.list[this.curr] + ')');
+    this.$display.css('background-image', 'none');
+
+    // Wait until the image has loaded.
+    if (!this.preloaded[this.curr].loaded) {
+      this.showLoadingMessage();
+      var self = this;
+      window.setTimeout(function() { self.show(); }, 100);
+      return;
+    }
+
+    this.hideLoadingMessage();
+    var size = this.getImageSize();
+    this.$display.css({
+      backgroundImage: 'url(' + this.list[this.curr] + ')',
+      width: size.width,
+      height: size.height,
+    });
+  };
+
+  /**
+   * Returns the width/height of the image specified by imageIndex after
+   * "background-size: contain" has resized it.
+   *
+   * @param  {int} imageIndex
+   *   Which image to check, defaults to this.curr
+   *
+   * @return {object}
+   *   Object with width and height members
+   */
+  Dennis.photos.getImageSize = function(imageIndex) {
+    if ('undefined' == typeof imageIndex) {
+      imageIndex = this.curr;
+    }
+
+    var image = this.preloaded[imageIndex];
+    var displayWidth = $('.dennis-two-col .fluid-column').width();
+    var displayHeight = $('.dennis-two-col .fluid-column').height();
+
+    return {
+      width: Math.min(displayWidth, displayHeight * image.aspectRatio),
+      height: Math.min(displayHeight, displayWidth / image.aspectRatio)
+    };
+  };
+
+  /**
+   * Shows and hides the "Loading..." message.
+   */
+  Dennis.photos.showLoadingMessage = function() {
+    this.$loading.show(100);
+  };
+  Dennis.photos.hideLoadingMessage = function() {
+    this.$loading.hide(350);
   };
 
   /**
@@ -102,17 +166,18 @@ var Dennis = Dennis || { photos: {} };
       // the loop has finished and we can't pass a parameter to the anonymous
       // function as it would call the function then, rather than after the
       // image has loaded. So, we tack on an attribute to the image object.
-      if (0 == i) {
-        image.closeLoadingMessage = true;
-      }
+      // if (0 == i) {
+      //   image.closeLoadingMessage = true;
+      // }
 
       image.onload = function() {
-        if (this.closeLoadingMessage) {
-          self.loading.fadeOut(350);
-        }
+        // if (this.closeLoadingMessage) {
+        //   self.$loading.fadeOut(350);
+        // }
 
         // Store aspect ratio for easy access later.
         this.aspectRatio = this.width / this.height;
+        this.loaded = true;
       }
 
       // Load image from the server. Since it's not part of the browser DOM, it
