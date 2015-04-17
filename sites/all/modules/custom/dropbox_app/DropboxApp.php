d6e4235ce30cedf0611d485e1512b0b4acfb4a9f
Mike Keran <info@mikekeran.com>
2015-04-17 12:37:57 +0200

    Moved most webAuth into __construct().

--- sites/all/modules/custom/dropbox_app/DropboxApp.php
+++ sites/all/modules/custom/dropbox_app/DropboxApp.php
@@ -5,30 +5,58 @@
 use \Dropbox;
 
 class DropboxApp {
+  /**
+   * @var \Dropbox\AppInfo
+   */
+  protected $appInfo;
 
   /**
-   * Loads the Dropbox SDK library and returns an AppInfo object.
-   *
-   * @return \Dropbox\AppInfo
-   * @throws \Exception
+   * @var \Dropbox\WebAuth
    */
-  function dropbox_app_load_sdk() {
-    // Only load the SDK once and cache the AppInfo return value.
-    static $appInfo = '';
+  protected $webAuth;
 
-    if (empty($appInfo)) {
-      $library = libraries_load('dropbox_sdk');
-      if (!$library || empty($library['loaded'])) {
-        throw new Exception($library['error message']);
-      }
+  /**
+   * @var string
+   *
+   * An identifier for the API client, typically of the form "Name/Version".
+   * This is used to set the HTTP User-Agent header when making API requests.
+   * Example: "Drupal-Photo-Gallery/1.0"
+   */
+  protected $clientId;
 
-      $appInfo = Dropbox\AppInfo::loadFromJson(array(
-        'key' => variable_get('dropbox_app_key', ''),
-        'secret' => variable_get('dropbox_app_secret', ''),
-      ));
+  /**
+   * @var string
+   *
+   * The URI that the Dropbox server will redirect the user to after the user
+   * finishes authorizing your app. This URI must be HTTPS-based and
+   * pre-registered with Dropbox, though "localhost"-based and "127.0.0.1"-based
+   * URIs are allowed without pre-registration and can be either HTTP or HTTPS.
+   * Generally, this endpoint will call authorizeFinish().
+   * Example: "https://www.example.com/dropbox/authorize/complete"
+   */
+  protected $redirectUri;
+
+
+  public function __construct($clientId, $redirectUri) {
+    $this->clientId = $clientId;
+    $this->redirectUri = $redirectUri;
+
+    // Load the Dropbox SDK library.
+    $library = libraries_load('dropbox_sdk');
+    if (!$library || empty($library['loaded'])) {
+      throw new Exception($library['error message']);
     }
 
-    return $appInfo;
+    // Collect App information. Note: even though the Dropbox API says it's
+    // loading "fromJSON," it's actually using the output from json_decode(),
+    // namely an array.
+    $this->appInfo = Dropbox\AppInfo::loadFromJson(array(
+      'key' => variable_get('dropbox_app_key', ''),
+      'secret' => variable_get('dropbox_app_secret', ''),
+    ));
+
+    $csrfTokenStore = new Dropbox\ArrayEntryStore($_SESSION, 'dropbox-auth-csrf-token');
+    $this->webAuth = new Dropbox\WebAuth($this->appInfo, $this->clientId, $this->redirectUrl, $csrfTokenStore);
   }
 
   protected function getWebAuth() {
